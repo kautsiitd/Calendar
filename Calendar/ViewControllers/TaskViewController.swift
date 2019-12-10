1b76643e21a62e8655e10cfc282ec5f29411e5ac
Kauts Kanu <kautscoding@gmail.com>
2019-12-10 23:22:51 +0530

    Added Icons

---
+++ Calendar/ViewControllers/TaskViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaskViewController.swift
+//  Calendar
+//
+//  Created by Kautsya Kanu on 21/11/19.
+//  Copyright © 2019 Kautsya Kanu. All rights reserved.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
